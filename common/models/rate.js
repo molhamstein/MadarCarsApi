e25fdfd1b15eb0ffcf2f058bdf3d0a582c924285
anas-1995 <anasalazmeh.95@gmail.com>
2019-02-07 11:31:07 +0200

    rate

--- common/models/rate.js
+++ common/models/rate.js
@@ -10,34 +10,68 @@ module.exports = function (Rate) {
    * @param {Function(Error, number)} callback
    */
 
-  Rate.makeRate = function (carId, tripId, value, req, callback) {
+  Rate.makeRate = function (carId, tripId, value, ownerId, req, callback) {
     var code = 200;
-    var ownerId = req.accessToken.userId;
-    Rate.app.models.car.findById(carId, function (err, oneCar) {
+    if (ownerId == undefined || ownerId == null)
+      ownerId = req.accessToken.userId;
+    Rate.app.models.trip.findById(tripId, function (err, oneTrip) {
       if (err)
         return callback(err, null);
-      if (oneCar == null)
-        return callback(errors.car.carNotFound());
-      Rate.create({
-        value: value,
-        carId: carId,
-        ownerId: ownerId,
-        tripId: tripId
-      }, function (err, data) {
+      Rate.app.models.car.findById(carId, function (err, oneCar) {
         if (err)
           return callback(err, null);
-        if (oneCar.numRateing == 0) {
-          oneCar.numRateing = 1;
+        if (oneCar == null)
+          return callback(errors.car.carNotFound());
+        Rate.create({
+          value: value,
+          carId: carId,
+          ownerId: ownerId,
+          tripId: tripId
+        }, function (err, data) {
+          if (err)
+            return callback(err, null);
+          if (oneCar.numRateing == 0) {
+            oneCar.numRateing = 1;
+            oneCar.rate = value;
+          } else {
+            oneCar.numRateing++;
+            oneCar.rate = (oneCar.rate + value) / 2;
+          }
+          oneCar.save();
+          oneTrip.rateId = data.id;
+          oneTrip.save();
+          return callback(null, code)
+        })
+      })
+    })
+  };
+
+  Rate.updateRate = function (id, carId, value, callback) {
+    Rate.findById(id, function (err, rate) {
+      if (err)
+        return callback(err, null);
+      var code = 200;
+      Rate.app.models.car.findById(carId, function (err, oneCar) {
+        if (err)
+          return callback(err, null);
+        if (oneCar == null)
+          return callback(errors.car.carNotFound());
+        if (oneCar.numRateing == 1) {
           oneCar.rate = value;
         } else {
-          oneCar.numRateing++;
+          oneCar.rate = (oneCar.rate * 2) - rate.value;
           oneCar.rate = (oneCar.rate + value) / 2;
         }
+        rate.value = value;
+        rate.save();
+        console.log("oneCar.rate")
+        console.log(oneCar.rate)
         oneCar.save();
         return callback(null, code)
       })
     })
   };
+
 
   /**
    *
